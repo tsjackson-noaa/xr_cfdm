83a004b4e5c986dd12a794c75ee797c2a3d6633d
Thomas Jackson <52828051+tsjackson-noaa@users.noreply.github.com>
2021-05-23 16:02:22 -0400

    Move unchanged cf-python/mixin modules

---
+++ xr_cfdm/cf_python/mixin/__init__.py
@@ -0,0 +1,11 @@
+# our modifications
+
+from ...propertiesdata import PropertiesData, Subspace
+from ...propertiesdatabounds import PropertiesDataBounds
+
+# unchanged from cf-python
+
+from .properties import Properties
+from .coordinate import Coordinate
+from .fielddomain import FieldDomain
+from .fielddomainlist import FieldDomainList
--- xr_cfdm/mixin/__init__.py
+++
@@ -1,6 +0,0 @@
-from .properties import Properties
-from .propertiesdata import PropertiesData, Subspace
-from .propertiesdatabounds import PropertiesDataBounds
-from .coordinate import Coordinate
-from .fielddomain import FieldDomain
-from .fielddomainlist import FieldDomainList
